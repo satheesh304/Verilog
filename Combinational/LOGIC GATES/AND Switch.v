4e252cfeb2d8bbcd17cf6bb6b2871a8fd54a3161
Satheesh <satheeshnarayan05@gmail.com>
2026-02-02 23:52:01 +0530

    Update AND Switch.v

--- Combinational/LOGIC GATES/AND Switch.v
+++ Combinational/LOGIC GATES/AND Switch.v
@@ -2,10 +2,11 @@ module and_sw(output y, input a, b);
 supply1 vdd; supply0 gnd;
 wire w;
 
-pmos p1(w, vdd, a);
-pmos p2(y, w, b);
-nmos n1(y, gnd, a);
-nmos n2(y, gnd, b);
+pmos p1(y,vdd, a);
+pmos p2(y,vdd, b);
+nmos n1(y, w, a);
+nmos n2(w, gnd, b);
+
 endmodule
 
 //TEST_BENCH
