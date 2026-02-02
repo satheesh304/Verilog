d194380f5fb53c59038b80c95610b0f8c082ae2d
satheesh304 <satheeshnarayan05@gmail.com>
2026-02-02 09:56:46 +0530

    Update AND Switch.v

--- Combinational/LOGIC GATES/AND Switch.v
+++ Combinational/LOGIC GATES/AND Switch.v
@@ -14,12 +14,11 @@ reg a,b;
 wire y;
 
 and_sw g1(y,a,b);
-
 initial begin
-a=0;b=0;
-#5 a=0;b=1;
-#5 a=1;b=0;
-#5 a=1;b=1;
-#5 $stop;
+    a=0;b=0;
+    #5 a=0;b=1;
+     #5 a=1;b=0;
+    #5 a=1;b=1;
+    #5 $stop;
 end
 endmodule
