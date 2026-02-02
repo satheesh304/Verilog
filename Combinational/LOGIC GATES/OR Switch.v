0521f674769fa1ba455f54e3e0ce05f902ac9724
Satheesh <satheeshnarayan05@gmail.com>
2026-02-03 00:00:23 +0530

    Update OR Switch.v

--- Combinational/LOGIC GATES/OR Switch.v
+++ Combinational/LOGIC GATES/OR Switch.v
@@ -2,25 +2,31 @@
 module or_sw(input a, b, output y);
 supply1 vdd; 
 supply0 gnd;
-wire w;
+wire w,y1;
 
-pmos p1(y, vdd, a);
-pmos p2(y, vdd, b);
-nmos n1(w, gnd, a);
-nmos n2(y, w, b);
+  pmos p1(w, vdd, a);
+  pmos p2(y1, w, b);
+  nmos n1(y1, gnd, a);
+  nmos n2(y1, gnd, b);
+
+    pmos p3(y,vdd, y1);
+    nmos n3(y, gnd, y1);
+
+  
 endmodule
 
-//test bench
-module tb_or;
+//TEST_BENCH
+module tb_and;
 reg a,b;
 wire y;
 
-or_sw g1(y,a,b);
+  or_sw g1(a,b,y);
 initial begin
-a=0;b=0;
-#100a=0;b=1;
-#100a=1;b=0;
-#100 a=1;b=1;
-#100 $stop;
+    a=0;b=0;
+  $monitor($time,"\tA=%b\t B=%b||\tOUTPUT=%b",a,b,y);
+    #5 a=0;b=1;
+     #5 a=1;b=0;
+    #5 a=1;b=1;
+    #5 $stop;
 end
 endmodule
