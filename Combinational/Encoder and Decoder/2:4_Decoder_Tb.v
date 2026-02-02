0c2a27fef03fa92b70fa6cd4560ab9022987ace7
Satheesh <satheeshnarayan05@gmail.com>
2026-02-02 23:22:31 +0530

    Update 2:4_Decoder_Tb.v

--- Combinational/Encoder and Decoder/2:4_Decoder_Tb.v
+++ Combinational/Encoder and Decoder/2:4_Decoder_Tb.v
@@ -1,5 +1,14 @@
 module Decoder_Tb();
   reg [1:0] a;
   wire [3:0] o;
-
+Decode d1(a,o);
+  
+  initial begin
+    a=2'b01;
+    $monitor($time,"\tA=%b\t||\tOUTPUT=%b",a,o);
+   #5 a=2'b10;
+   #5 a=2'b00;
+   #5 a=2'b11;
+  end
+endmodule
   
