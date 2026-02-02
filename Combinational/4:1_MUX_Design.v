417e74fdb1593e7a3c6f82da38dcfb445247fc1d
Satheesh <satheeshnarayan05@gmail.com>
2026-02-02 16:27:05 +0530

    Update 4:1_MUX_Design.v

--- Combinational/4:1_MUX_Design.v
+++ Combinational/4:1_MUX_Design.v
@@ -4,5 +4,6 @@ input [3:0] a;
 input s1,s2;
   
   assign out = (s1==0 && s2==0) ? a[0] :(s1==0 && s2==1) ? a[1] :(s1==1 && s2==0) ? a[2]:a[3];
+  assign out = (s1==0)?((s2==0?a[0]:a[1])):((s2==0?a[2]:a[3]));
 endmodule
 
