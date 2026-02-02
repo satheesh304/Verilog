03378856ae4976bd61c1f65ffcc2ebfcb0d30a5c
satheesh304 <satheeshnarayan05@gmail.com>
2026-02-02 12:19:45 +0530

    Update Full adder.v

--- Combinational/Full adder.v
+++ Combinational/Full adder.v
@@ -8,19 +8,4 @@ assign w2 = a & b;
 assign w3 = w1 & cin;
 assign carry = w2 | w3;
 endmodule
-//TEST BENCH
-module tb_full;
-reg a,b,cin;
-wire sum,carry;
 
-full_adder f1(sum,carry,a,b,cin);
-
-initial begin
-a=0;b=0;cin=0;
-#10 a=0;b=1;cin=0;
-#10 a=1;b=0;cin=0;
-#10 a=1;b=1;cin=0;
-#10 a=1;b=1;cin=1;
-#10 $stop;
-end
-endmodule
