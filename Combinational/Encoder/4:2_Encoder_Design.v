1b24a68bf412a673433a76156a607374b4f12c9b
Satheesh <satheeshnarayan05@gmail.com>
2026-02-02 19:10:19 +0530

    Update 4:2_Encoder_Design.v

--- Combinational/Encoder/4:2_Encoder_Design.v
+++ Combinational/Encoder/4:2_Encoder_Design.v
@@ -2,7 +2,7 @@ module Encoder(a,o);
   input [3:0] a;
   output [1:0] o;
   
-assign o = (a[0]) ? 2'b00 :
+  assign o = (a[0]) ? 2'b00 :         //assign o[1]=a[3] |a[2] ;   assign o[0]=a[2]|a[1];
            (a[1]) ? 2'b01 :
            (a[2]) ? 2'b10 :
            (a[3]) ? 2'b11 :
