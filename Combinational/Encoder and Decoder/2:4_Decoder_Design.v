caa86d78b09a0547894a424a111ca842a39cc84b
Satheesh <satheeshnarayan05@gmail.com>
2026-02-02 23:29:58 +0530

    Update 2:4_Decoder_Design.v

--- Combinational/Encoder and Decoder/2:4_Decoder_Design.v
+++ Combinational/Encoder and Decoder/2:4_Decoder_Design.v
@@ -2,6 +2,6 @@ module Decoder(a,o);
   input [1:0] a;
   output [3:0] o;
 
-  assign o=(~a[0]*~a[1])+(a[0]*~a[1])+(~a[0]*a[1])+(a[0]*a[1]);
-  //assign o= a[1]?(a[0]?o[3]=1:o[2]=1):(a[0]?o[1]=1:o[0]=1)
+  //assign o=(~a[0]*~a[1])+(a[0]*~a[1])+(~a[0]*a[1])+(a[0]*a[1]);
+  assign o= a[1]?(a[0]?4'b1000:4'b0100):(a[0]?4'b0010:4'b0001);
 endmodule
