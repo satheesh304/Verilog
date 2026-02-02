36f5b91e2391ce0bb0f60409589a0eec4d4b1f20
satheesh304 <satheeshnarayan05@gmail.com>
2026-02-02 10:10:14 +0530

    Update Adder\Subtractor.v

--- Combinational/Adder\Subtractor.v
+++ Combinational/Adder\Subtractor.v
@@ -1,4 +1,4 @@
-module add_sub(a, b, mode,sum,carry);
+module add/sub(a, b, mode,sum,carry);
 output [3:0] sum;
 output carry;
 input [3:0] a,b;
@@ -8,11 +8,11 @@ input mode;
   wire c1,c2,c3;
 
 assign new_b = b ^ {4{mode}};
-
-full_adder f0(sum[0],c1,a[0],new_b[0],mode);
-full_adder f1(sum[1],c2,a[1],new_b[1],c1);
-full_adder f2(sum[2],c3,a[2],new_b[2],c2);
-full_adder f3(sum[3],carry,a[3],new_b[3],c3);
+  
+  full_adder f0(a[0],new_b[0],mode,sum[0],c1);
+  full_adder f1(a[1],new_b[1],c1,sum[1],c2);
+  full_adder f2(a[2],new_b[2],c2,sum[2],c3);
+  full_adder f3(a[3],new_b[3],c3,sum[3],carry);
 endmodule
 
 module tb_add_sub;
@@ -21,7 +21,7 @@ reg mode;
 wire [3:0] sum;
 wire carry;
 
-add_sub a1(sum,carry,a,b,mode);
+  add/sub a1(a,b,mode,sum,carry,);
 
 initial begin
 a=4'b0101; b=4'b0011; mode=0; // ADD
