8ae25ed907954daa763f07e91fcb6338c93eed9e
Satheesh <satheeshnarayan05@gmail.com>
2026-02-03 17:34:35 +0530

    Update Full_Adder using Half_Adder_Design..v

--- Combinational/Arithmatic/Full_Adder using Half_Adder_Design/Full_Adder using Half_Adder_Design..v
+++ Combinational/Arithmatic/Full_Adder using Half_Adder_Design/Full_Adder using Half_Adder_Design..v
@@ -4,7 +4,7 @@ module FA_U_HA(input a,b,cin,output sum,carry);
 wire s1,c1,c2;
   
   half_adder ha1(s1,c1, a, b);
-  half_adder(sum, c2, s1, cin);
+  half_adder ha2(sum, c2, s1, cin);
   or (carry,c1,c2);
 
 endmodule
