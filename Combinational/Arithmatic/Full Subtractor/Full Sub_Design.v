a116980083a33a616ddca95a0766a4e6ac067e99
Satheesh <satheeshnarayan05@gmail.com>
2026-02-03 17:58:31 +0530

    Update Full Sub_Design.v

--- Combinational/Arithmatic/Full Subtractor/Full Sub_Design.v
+++ Combinational/Arithmatic/Full Subtractor/Full Sub_Design.v
@@ -11,19 +11,4 @@ wire w1,w2,w3;
 ;
 endmodule
 
-//TEST BENCH
-module tb_full_sub;
-reg a,b,cin;
-wire diff, borrow;
 
-  full_sub f1(a,b,cin,diff,borrow);
-
-initial begin
-a=0;b=0;cin=0;
-#10 a=0;b=1;cin=0;
-#10 a=1;b=0;cin=0;
-#10 a=1;b=1;cin=0;
-#10 a=1;b=1;cin=1;
-#10 $stop;
-end
-endmodule
