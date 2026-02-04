57ec96c8cac6ffca6b2577148ae3c0f176ca8d62
Satheesh <satheeshnarayan05@gmail.com>
2026-02-04 12:22:26 +0530

    Update 1:4_DeMUX.v

--- Combinational/MUX and DeMUX/1:4_DeMUX/1:4_DeMUX.v
+++ Combinational/MUX and DeMUX/1:4_DeMUX/1:4_DeMUX.v
@@ -1,8 +1,11 @@
-module MUX_4X1 (a,s1,s2,out);
+module DeMUX_4x1 (a,s1,s2,out);
 output [3:0] out;
 input  a;
 input s1,s2;
   
-  assign out = (s1==0 && s2==0) ? out[0]=a :(s1==0 && s2==1) ? out[1]=a :(s1==1 && s2==0) ? out[2]=a:out[3]=a;
-  //assign out = (s1==0)?((s2==0?out[0]=a:out[1]=a)):((s2==0?out[2]=a:out[3]=a));
+  assign out[0] =(s1==0 && s2==0) ? a:1'b0;
+  assign out[1] =(s1==0 && s2==1) ? a:1'b0;
+  assign out[2] =(s1==1 && s2==0) ? a:1'b0;
+  assign out[3] =(s1==1 && s2==1) ? a:1'b0;
+ 
 endmodule
