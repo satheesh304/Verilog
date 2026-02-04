6a53faf8b01dca4e954b7738d8558d9fa4638aec
Satheesh <satheeshnarayan05@gmail.com>
2026-02-04 12:29:22 +0530

    Update 1:8_DeMUX Using 1:4_DeMUX_Design.v

--- Combinational/MUX and DeMUX/1:8_DeMUX Using 1:4_DeMUX/1:8_DeMUX Using 1:4_DeMUX_Design.v
+++ Combinational/MUX and DeMUX/1:8_DeMUX Using 1:4_DeMUX/1:8_DeMUX Using 1:4_DeMUX_Design.v
@@ -1,1 +1,17 @@
+`include "2:1_MUX.v"
+`include "4:1_MUX.v"
 
+
+module MUX(a,s1,s2,s3,out);
+output [7:0] out;
+input a;
+input s1,s2,s3;
+wire [1:0] o;
+  
+  DeMUX_2x1 mux1(a,s1,o[1:0]);
+  
+  DeMUX_4x1 mux1(o[0],s2,s3,out[3:0]);
+  DeMUX_4x1 mux2(o[1],s2,s3,out[7:4]);
+  
+  
+endmodule
