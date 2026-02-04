ed031cf9bea41c40e3bb88b63cb3605fb288ba1b
Satheesh <satheeshnarayan05@gmail.com>
2026-02-04 11:52:34 +0530

    Update 8:1_MUX Using 4:1_MUX_Tb.v

--- Combinational/MUX and DeMUX/8:1_MUX Using 4:1_MUX/8:1_MUX Using 4:1_MUX_Tb.v
+++ Combinational/MUX and DeMUX/8:1_MUX Using 4:1_MUX/8:1_MUX Using 4:1_MUX_Tb.v
@@ -9,7 +9,7 @@ initial begin
   a=8'b11001101; s1=0; s2=0; s3=0;
   $monitor("TIME=%t ||  A=%b ||  s1=%b  s2=%b s3=%b|| output=%b",$time,a,s1,s2,s3,out);
  
-  #10 s1=0; s2=0; s3=0;
+ 
   #10 s1=0; s2=0; s3=1;
   #10 s1=0; s2=1; s3=0;
   #10 s1=0; s2=1; s3=1;
