e3b438bb0d00d41ef9a20d61bfdb40e9dd08b99c
Satheesh <satheeshnarayan05@gmail.com>
2026-02-04 11:07:59 +0530

    Update 8:1_MUX Using 4:1_MUX Design.v

--- Combinational/MUX and DeMUX/8:1_MUX Using 4:1_MUX/8:1_MUX Using 4:1_MUX Design.v
+++ Combinational/MUX and DeMUX/8:1_MUX Using 4:1_MUX/8:1_MUX Using 4:1_MUX Design.v
@@ -1,1 +1,8 @@
-
+module MUX(a,s1,s2,s3,out);
+output out;
+input [7:0] a;
+input s1,s2,s3;
+  
+  assign out = (s1==0 && s2==0) ? a[0] :(s1==0 && s2==1) ? a[1] :(s1==1 && s2==0) ? a[2]:a[3];
+  //assign out = (s1==0)?((s2==0?a[0]:a[1])):((s2==0?a[2]:a[3]));
+endmodule
