d3061ce12e426b210b49b52b1d3b255f5290133a
Satheesh <satheeshnarayan05@gmail.com>
2026-02-04 19:28:57 +0530

    Create 3:8_Decoder_Design.v

---
+++ Combinational/Encoder and Decoder/3:8_Decoder/3:8_Decoder_Design.v
@@ -0,0 +1,8 @@
+module Decoder(a,o);
+  input [1:0] a;
+  output [3:0] o;
+
+  //assign o=(~a[0]*~a[1])+(a[0]*~a[1])+(~a[0]*a[1])+(a[0]*a[1]);
+  //assign o= 4'b0001<<a;
+  assign o= a[1]?(a[0]?4'b1000:4'b0100):(a[0]?4'b0010:4'b0001);
+endmodule
