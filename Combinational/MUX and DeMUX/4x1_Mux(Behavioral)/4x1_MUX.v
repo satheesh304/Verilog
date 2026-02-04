963396c7514df9544c0ad62e038671038f0e4e8e
Satheesh <satheeshnarayan05@gmail.com>
2026-02-04 14:21:51 +0530

    Update 4x1_MUX.v

--- Combinational/MUX and DeMUX/4x1_Mux(Behavioral)/4x1_MUX.v
+++ Combinational/MUX and DeMUX/4x1_Mux(Behavioral)/4x1_MUX.v
@@ -4,7 +4,13 @@ module mux(a,s1,s2,out);
   output out;
 
   always @(*) begin
-    out = (s1==0 && s2==0) ? a[0] :(s1==0 && s2==1) ? a[1] :(s1==1 && s2==0) ? a[2]:a[3];
+    //out = (s1==0 && s2==0) ? a[0] :(s1==0 && s2==1) ? a[1] :(s1==1 && s2==0) ? a[2]:a[3];
+    case ({s1,s2})
+    2'b00: y = a[0];
+    2'b01: y = a[1];
+    2'b10: y = a[2];
+    2'b11: y = a[3];
+    default: y = 1'b0;
   end
 
 endmodule
