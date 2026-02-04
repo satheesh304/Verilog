8536ca6d2a99806041f4a66d4fe092951522eb0c
Satheesh <satheeshnarayan05@gmail.com>
2026-02-04 12:09:22 +0530

    Update 1:4_DeMUX_Tb.v

--- Combinational/MUX and DeMUX/1:4_DeMUX/1:4_DeMUX_Tb.v
+++ Combinational/MUX and DeMUX/1:4_DeMUX/1:4_DeMUX_Tb.v
@@ -1,1 +1,24 @@
 
+module tb_MUX_4x1;
+reg  a;
+reg s1,s2;
+wire [3:0] out;
+
+   MUX_4x1 m1 (a,s1,s2,out);
+  
+initial begin
+  a=1'b1; s1=0; s2=0;
+ $monitor("TIME=%t ||  A=%b ||  s1=%b  s2=%b || output=%b",$time,a,s1,s2,out);
+  #10 ; s1=0; s2=1;
+  #10  s1=1; s2=0;
+  #10  s1=1; s2=1;
+  end
+  
+  initial begin
+    $dumpfile("mux.vcd");
+    $dumpvars;
+    #100
+    $finish;
+  end
+endmodule
+
