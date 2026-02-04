4ddc648ac6acb9d51a0daac9fa28a506dd3be55f
Satheesh <satheeshnarayan05@gmail.com>
2026-02-04 19:55:44 +0530

    Update 3:8_Decoder_Tb.v

--- Combinational/Encoder and Decoder/3:8_Decoder/3:8_Decoder_Tb.v
+++ Combinational/Encoder and Decoder/3:8_Decoder/3:8_Decoder_Tb.v
@@ -1,13 +1,18 @@
-module Decoder_Tb();
-  reg [1:0] a;
-  wire [3:0] o;
+module Decoder_tb();
+  reg [2:0] a;
+  wire [7:0] o;
+  
 Decoder d1(a,o);
   
   initial begin
-    a=2'b01;
+    a=3'b000;
     $monitor($time,"\tA=%b\t||\tOUTPUT=%b",a,o);
-   #5 a=2'b10;
-   #5 a=2'b00;
-   #5 a=2'b11;
+     #5 a=3'b001;
+     #5 a=3'b010;
+     #5 a=3'b011;
+     #5 a=3'b100;
+     #5 a=3'b101;
+     #5 a=3'b110;
+     #5 a=3'b111;
   end
 endmodule
