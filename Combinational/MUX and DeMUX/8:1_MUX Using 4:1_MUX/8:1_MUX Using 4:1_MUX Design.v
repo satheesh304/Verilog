601a998d445434d991f38cfd606157eda29fe516
Satheesh <satheeshnarayan05@gmail.com>
2026-02-04 11:24:23 +0530

    Update 8:1_MUX Using 4:1_MUX Design.v

--- Combinational/MUX and DeMUX/8:1_MUX Using 4:1_MUX/8:1_MUX Using 4:1_MUX Design.v
+++ Combinational/MUX and DeMUX/8:1_MUX Using 4:1_MUX/8:1_MUX Using 4:1_MUX Design.v
@@ -5,9 +5,10 @@ module MUX(a,s1,s2,s3,out);
 output out;
 input [7:0] a;
 input s1,s2,s3;
-wire oot1,out2;
+  wire [1:0] o;
 
-  MUX_2X1 mux1() 
+  MUX_4X1 mux1(a[3:0],s1,s2,o[0]);
+  MUX_4X1 mux2(a[7:4],s1,s2,o[1]);
   
-
+  MUX_2X1 mux3(o,s3,out);
 endmodule
