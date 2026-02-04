638ace182e2c3b94cf3ca5ebedd3e27219f5797d
Satheesh <satheeshnarayan05@gmail.com>
2026-02-04 20:17:55 +0530

    Create Binary_to_Gray_Tb.v

---
+++ Combinational/CODE CONVERTERS/Binary_to_Gray/Binary_to_Gray_Tb.v
@@ -0,0 +1,17 @@
+module Binary_to_Gray_Tb;
+  reg [3:0] a;
+  wire [3:0] o;
+  integer i;
+  
+ Binary_to_Gray b1(a,o);
+  
+  initial begin
+ 
+    $monitor($time,"\tA=%b\t||\tOUTPUT=%b",a,o);
+    for(i=0;i<16;i=i+1)
+      begin
+      a=i;
+      #10;
+      end
+  end
+endmodule
