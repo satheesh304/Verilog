84472028116357a0bc03c4bec0e45f1db04d4982
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 19:07:36 +0530

    Update Odd_parity_check_Tb.v

--- Combinational/Comparators and Error Detect/5)Parity_Checker/Odd_Parity/Odd_parity_check_Tb.v
+++ Combinational/Comparators and Error Detect/5)Parity_Checker/Odd_Parity/Odd_parity_check_Tb.v
@@ -1,1 +1,28 @@
-
+module Odd_Par_CHECK_tb();
+  reg [3:0] Data;
+  reg Odd_parity;
+  wire OPCheck;
+  integer i;
+  
+  Odd_Par_CHECK  Oc1(Data,Odd_parity, OPCheck);
+  
+  initial begin
+    
+    for(i=0;i<32;i=i+1)
+      begin
+        {Odd_parity,Data}=i;
+        if( OPCheck) 
+          $display($time,"\tA=%b\t||\tEven_Parity=%b\t||\tEPCheck=%b\t There is ERROR in the CODE",Data,Odd_parity,OPCheck);
+        else
+          $display($time,"\tA=%b\t||\tEven_Parity=%b\t||\tEPCheck=%b\t There is NO-ERROR in the CODE",Data,Odd_parity,OPCheck);
+        
+      #10;
+      end
+  end
+   initial begin
+    $dumpfile("mux.vcd");
+    $dumpvars;
+    #1000
+    $finish;
+  end
+endmodule
