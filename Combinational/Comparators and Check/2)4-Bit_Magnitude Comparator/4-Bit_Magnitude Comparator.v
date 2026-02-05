95e7d0107700f3f31f9c20e095584df5e9c5145e
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 11:57:49 +0530

    Update 4-Bit_Magnitude Comparator.v

--- Combinational/Comparators and Check/2)4-Bit_Magnitude Comparator/4-Bit_Magnitude Comparator.v
+++ Combinational/Comparators and Check/2)4-Bit_Magnitude Comparator/4-Bit_Magnitude Comparator.v
@@ -1,1 +1,11 @@
-
+//Design
+module Compare_4bit(a,b,o);
+  input [3:0] a,b;
+  output reg [1:0] o;
+  
+  always @(*)
+    o=(a>b)?2'b01:
+      (a<b)?2'b10:
+      (a==b)?2'b11:
+       2'b00;
+endmodule
