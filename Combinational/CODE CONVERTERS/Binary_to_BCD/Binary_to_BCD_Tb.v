8aa220b57eb6566c17d2486e8354c56fa736df18
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 06:37:36 +0530

    Update Binary_to_BCD_Tb.v

--- Combinational/CODE CONVERTERS/Binary_to_BCD/Binary_to_BCD_Tb.v
+++ Combinational/CODE CONVERTERS/Binary_to_BCD/Binary_to_BCD_Tb.v
@@ -1,1 +1,25 @@
-
+module Binary_to_BCD_Tb;
+  reg [15:0] a;
+  wire [3:0] o [3:0];
+  integer i;
+  
+ Binary_to_BCD b1(a,o);
+  
+  initial begin
+ 
+    
+    for(i=0;i<50;i=i+1)
+      begin
+         $display($time,"\tA=%b\t||\tOUTPUT=%b  %b  %b  %b",a,o[3],o[2],o[1],o[0]);
+      a=i;
+       
+      #10;
+      end
+  end
+   initial begin
+    $dumpfile("mux.vcd");
+    $dumpvars;
+    #1000
+    $finish;
+  end
+endmodule
