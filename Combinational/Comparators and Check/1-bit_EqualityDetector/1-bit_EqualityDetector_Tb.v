b6e6c273af43592d9534512738ce35e06a6553b3
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 11:38:44 +0530

    Create 1-bit_EqualityDetector_Tb.v

---
+++ Combinational/Comparators and Check/1-bit_EqualityDetector/1-bit_EqualityDetector_Tb.v
@@ -0,0 +1,29 @@
+module equality_Tb;
+  reg a,b;
+  wire o;
+  integer i,j;
+  Equality e1( a,b,o);
+  
+  initial begin
+   
+    for(i=0;i<2;i+=1)begin
+      for(j=0;j<2;j+=1)begin
+        a=i;
+        b=j;
+        #10;
+        if (o)
+          $display("Time=%0t ||a=%b  b=%b || a and b are equal",$time,a,b);
+        else
+          $display("Time=%0t ||a=%b  b=%b || a and b are not equal",$time,a,b);
+        
+      end
+    end
+  end
+  initial begin
+    $dumpfile("mux.vcd");
+    $dumpvars;
+    #1000
+    $finish;
+  end
+endmodule
+    
