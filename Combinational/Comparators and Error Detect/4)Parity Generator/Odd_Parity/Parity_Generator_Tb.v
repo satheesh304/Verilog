360ec3463fa2101cae4f7415fd39251a79eff16b
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 18:28:22 +0530

    Update Parity_Generator_Tb.v

--- Combinational/Comparators and Error Detect/4)Parity Generator/Odd_Parity/Parity_Generator_Tb.v
+++ Combinational/Comparators and Error Detect/4)Parity Generator/Odd_Parity/Parity_Generator_Tb.v
@@ -1,1 +1,22 @@
-
+module Odd_Par_Gen_tb();
+  reg [3:0] Data;
+  wire  Op;
+  integer i;
+  
+Odd_Par_Gen op1(Data,Op);
+  
+  initial begin
+    $monitor($time,"\tA=%b\t||\tOUTPUT=%b",Data,Op);
+    for(i=0;i<16;i=i+1)
+      begin
+      Data=i;
+      #10;
+      end
+  end
+   initial begin
+    $dumpfile("mux.vcd");
+    $dumpvars;
+    #1000
+    $finish;
+  end
+endmodule
