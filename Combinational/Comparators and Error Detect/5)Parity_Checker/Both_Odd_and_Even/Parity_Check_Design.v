c0695188274c28c14a0c0754aac4eb8eceab5c1e
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 20:12:25 +0530

    Update Parity_Check_Design.v

--- Combinational/Comparators and Error Detect/5)Parity_Checker/Both_Odd_and_Even/Parity_Check_Design.v
+++ Combinational/Comparators and Error Detect/5)Parity_Checker/Both_Odd_and_Even/Parity_Check_Design.v
@@ -1,1 +1,10 @@
+module OddEven_Par_CHECK(Data,Even_parity,Odd_Parity, EpCheck,OpCheck);
+  input [3:0] Data;
+  input Even_parity,Odd_Parity;
+  output EpCheck,OpCheck ;
+  
+  assign EpCheck=Even_parity^Data[0]^Data[1]^Data[2]^Data[3];
+  assign OpCheck=~(Odd_Parity^Data[0]^Data[1]^Data[2]^Data[3]);
+ 
+endmodule
 
