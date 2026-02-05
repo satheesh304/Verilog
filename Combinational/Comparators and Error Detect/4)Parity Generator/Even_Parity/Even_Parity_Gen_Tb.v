1a0a7338172ddf470ff8e484e38ac1226d8bae3d
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 18:30:26 +0530

    Create Even_Parity_Gen_Tb.v

---
+++ Combinational/Comparators and Error Detect/4)Parity Generator/Even_Parity/Even_Parity_Gen_Tb.v
@@ -0,0 +1,22 @@
+module Even_Par_Gen_tb();
+  reg [3:0] Data;
+  wire  Ep;
+  integer i;
+  
+Even_Par_Gen op1(Data,Op);
+  
+  initial begin
+    $monitor($time,"\tA=%b\t||\tEven_Parity=%b",Data,Ep);
+    for(i=0;i<16;i=i+1)
+      begin
+      Data=i;
+      #10;
+      end
+  end
+   initial begin
+    $dumpfile("mux.vcd");
+    $dumpvars;
+    #1000
+    $finish;
+  end
+endmodule
