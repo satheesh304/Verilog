0daf7c87bec7b972fda4e71b2496d8e2130e2499
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 18:59:33 +0530

    Update Even_Parity_check_Tb.v

--- Combinational/Comparators and Error Detect/5)Parity_Checker/Even_Parity/Even_Parity_check_Tb.v
+++ Combinational/Comparators and Error Detect/5)Parity_Checker/Even_Parity/Even_Parity_check_Tb.v
@@ -1,1 +1,28 @@
-
+module Even_Par_CHECK_tb();
+  reg [3:0] Data;
+  reg Even_parity;
+  wire EPCheck;
+  integer i;
+  
+  Even_Par_CHECK EC1(Data,Even_parity, EPCheck);
+  
+  initial begin
+    
+    for(i=0;i<32;i=i+1)
+      begin
+        {Even_parity,Data}=i;
+        if( EPCheck) 
+          $display($time,"\tA=%b\t||\tEven_Parity=%b\t||\tEPCheck=%b\t There is ERROR in the CODE",Data,Even_parity,EPCheck);
+        else
+          $display($time,"\tA=%b\t||\tEven_Parity=%b\t||\tEPCheck=%b\t There is NO-ERROR in the CODE",Data,Even_parity,EPCheck);
+        
+      #10;
+      end
+  end
+   initial begin
+    $dumpfile("mux.vcd");
+    $dumpvars;
+    #1000
+    $finish;
+  end
+endmodule
