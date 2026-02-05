cd962a56972642eca97813b377450c73f6f21f4a
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 06:05:52 +0530

    Update Gray_to_Binary_Design.v

--- Combinational/CODE CONVERTERS/Gray_to_Binary/Gray_to_Binary_Design.v
+++ Combinational/CODE CONVERTERS/Gray_to_Binary/Gray_to_Binary_Design.v
@@ -1,1 +1,15 @@
+//Design
+module Gray_to_Binary (gray,binary);
+  output reg [3:0] binary;
+  input [3:0] gray;
+  integer i;
 
+ 
+  always@(*)begin
+    binary[3]=gray[3];
+   for (i=3;i>0;i=i-1)begin
+     binary[i-1]=binary[i]^gray[i-1];
+  end
+ end
+  
+endmodule
