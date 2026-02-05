db20bec395eee24da00893dc7731ebb9b4bc4883
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 11:09:20 +0530

    Update BCD_to_Excess-3_Design.v

--- Combinational/CODE CONVERTERS/BCD_to_Excess-3/BCD_to_Excess-3_Design.v
+++ Combinational/CODE CONVERTERS/BCD_to_Excess-3/BCD_to_Excess-3_Design.v
@@ -1,21 +1,18 @@
-module BCD_to_Ex3(BCD,Ex3);
-  input [15:0] BCD;
+//Design
+`include "Binary_to_BCD.v"
+module Binary_to_Ex3(Binary,Ex3);
+  input [15:0] Binary;
   output reg [15:0] Ex3;
+  wire [15:0] BCD;
+  
+  Binary_to_BCD bB(Binary,BCD);
   
   always @(*)begin
-    Ex3=BCD+ 4'b 0011;
+    Ex3[3:0]=BCD[3:0]+ 4'b 0011;
+    Ex3[7:4]=BCD[7:4]+ 4'b 0011;
+    Ex3[11:8]=BCD[11:8]+ 4'b 0011;
+    Ex3[15:12]=BCD[15:12]+ 4'b 0011;
     
-    if (Ex3[3:0]>10) begin
-       Ex3[3:0]=4'b1001;
-       Ex3[7:4]+=1;
-       if (Ex3[7:4]>10)begin
-          Ex3[7:4]=4'b1001;
-          Ex3[11:8]+=1;
-          if (Ex3[11:8]>10)begin
-            Ex3[11:8]=4'b1001;
-            Ex3[15:12]+=1;
-        end
-      end
-    end
   end
 endmodule
+      
