5f3a6dd32a8671700b2875338a1b0a949100b8e8
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 18:50:07 +0530

    Update Nx1_MUX_Tb.v

--- Combinational/MUX and DeMUX/Nx1_MUX/Nx1_MUX_Tb.v
+++ Combinational/MUX and DeMUX/Nx1_MUX/Nx1_MUX_Tb.v
@@ -4,18 +4,17 @@ module tb_MUX #(parameter N=8,parameter Sel=3);
   wire out;
 
   N_MUX n1(a,s,out);
-  
+
 initial begin
-  a=8'b11001101;  s=3'b000;
-  $monitor("TIME=%0t ||  A=%b ||  s=%b || output=%b",$time,a,s,out);
-  #10 s=3'b001;
-  #10 s=3'b010;
-  #10 s=3'b011;
-  #10 s=3'b100;
-  #10 s=3'b101;
-  #10 s=3'b110;
-  #10 s=3'b111;
+ 
+  $monitor($time,"\tA=%b\t||\ts=%b\t||OUTPUT=%b ",$time,a,s,out);
+    for(i=0;i<50;i=i+1)
+      begin
+      s=i;
+      #10;
+      end
   end
+
   
   initial begin
     $dumpfile("mux.vcd");
