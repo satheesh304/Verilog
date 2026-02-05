760b2072e74c8ea7bd4e5023b9d3d0f47cb2aa37
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 19:07:04 +0530

    Update Odd_parity_check_Design.v

--- Combinational/Comparators and Error Detect/5)Parity_Checker/Odd_Parity/Odd_parity_check_Design.v
+++ Combinational/Comparators and Error Detect/5)Parity_Checker/Odd_Parity/Odd_parity_check_Design.v
@@ -1,1 +1,8 @@
-
+module Odd_Par_CHECK(Data,Odd_parity, OpCheck);
+  input [3:0] Data;
+  input Odd_parity;
+  output reg  OpCheck ;
+  
+  assign OpCheck=Odd_parity~^Data[0]~^Data[1]~^Data[2]~^Data[3];
+ 
+endmodule
