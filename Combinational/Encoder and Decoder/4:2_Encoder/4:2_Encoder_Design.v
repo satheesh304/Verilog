56ee3a6ead9e96e1fe183c6fab62e9983290b615
Satheesh <satheeshnarayan05@gmail.com>
2026-02-06 01:59:50 +0530

    Update 4:2_Encoder_Design.v

--- Combinational/Encoder and Decoder/4:2_Encoder/4:2_Encoder_Design.v
+++ Combinational/Encoder and Decoder/4:2_Encoder/4:2_Encoder_Design.v
@@ -1,11 +1,12 @@
-module Encoder(a,o);
-  input [3:0] a;
+module Encoder(a, o);
+  input  [3:0] a;
   output [1:0] o;
-  
-  assign o = (a[0]) ? 2'b00 :         //assign o[1]=a[3] |a[2] ;   
-           (a[1]) ? 2'b01 :           //assign o[0]=a[2]|a[1];
-           (a[2]) ? 2'b10 :
-           (a[3]) ? 2'b11 :
-            2'bxx;
+
+  assign o = (a == 4'b0001) ? 2'b00 :
+             (a == 4'b0010) ? 2'b01 :
+             (a == 4'b0100) ? 2'b10 :
+             (a == 4'b1000) ? 2'b11 :
+                              2'bxx;   // invalid cond 0 or more than one bit = 1
 
 endmodule
+
