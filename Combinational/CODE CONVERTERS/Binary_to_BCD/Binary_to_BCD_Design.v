6c95d1e681d8f5d474110b423f8e0c278200eb74
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 09:41:14 +0530

    Update Binary_to_BCD_Design.v

--- Combinational/CODE CONVERTERS/Binary_to_BCD/Binary_to_BCD_Design.v
+++ Combinational/CODE CONVERTERS/Binary_to_BCD/Binary_to_BCD_Design.v
@@ -1,14 +1,14 @@
 module Binary_to_BCD (binary,BCD);
-  input [15:0] binary;
-  output reg [3:0] BCD [3:0];
+  input [15:0] binary; 
+  output reg [3:0] BCD [3:0];  //[15:0] BCD;
   //wire [3:0] ones,tens,hunds,thous;
 
  
   always@(*)begin
-    BCD[0]= binary % 10;
-    BCD[1]= (binary / 10) % 10;
-    BCD[2]= binary / 100;
-    BCD[3]=(binary/100)%10;    
+    BCD[0]= binary % 10;            //BCD[3:0]= binary % 10;
+    BCD[1]= (binary / 10) % 10;     //BCD[7:4]= (binary / 10) % 10;
+    BCD[2]= binary / 100;           //BCD[11:8]= binary / 100;
+    BCD[3]=(binary/100)%10;         //BCD[15:12]=(binary/100)%10; 
  end
   
 endmodule
