153fd34889324e2a9dec1da9f4ea742d3cd131aa
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 18:59:06 +0530

    Update Even_Parity_check_Design.v

--- Combinational/Comparators and Error Detect/5)Parity_Checker/Even_Parity/Even_Parity_check_Design.v
+++ Combinational/Comparators and Error Detect/5)Parity_Checker/Even_Parity/Even_Parity_check_Design.v
@@ -1,1 +1,8 @@
-
+module Even_Par_CHECK(Data,Even_parity, EPCheck);
+  input [3:0] Data;
+  input Even_parity;
+  output reg  EPCheck ;
+  
+  assign EPCheck=Even_parity^Data[0]^Data[1]^Data[2]^Data[3];
+ 
+endmodule
