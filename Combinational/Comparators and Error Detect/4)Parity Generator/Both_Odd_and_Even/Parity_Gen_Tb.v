d27546fb4ce20301cd00c3614d5ad145be9124ac
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 19:16:40 +0530

    Create Parity_Gen_Tb.v

---
+++ Combinational/Comparators and Error Detect/4)Parity Generator/Both_Odd_and_Even/Parity_Gen_Tb.v
@@ -0,0 +1,22 @@
+module Even_Par_Gen_tb();
+  reg [3:0] Data;
+  wire  Ep,Op;
+  integer i;
+  
+  Odd-Even_Par_Gen op1(Data,Ep,Op);
+  
+  initial begin
+    $monitor($time,"\tA=%b\t||\tEven_Parity=%b\t||\tOdd_Parity=%b",Data,Ep,Op);
+    for(i=0;i<16;i=i+1)
+      begin
+      Data=i;
+      #10;
+      end
+  end
+   initial begin
+     $dumpfile("Even-Odd.vcd");
+    $dumpvars;
+    #1000
+    $finish;
+  end
+endmodule
