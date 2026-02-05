abd9fb18631b2d865b01b34ea60700c7d258375a
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 06:56:23 +0530

    Update BCD_to_Binary_Design.v

--- Combinational/CODE CONVERTERS/BCD_to_Binary/BCD_to_Binary_Design.v
+++ Combinational/CODE CONVERTERS/BCD_to_Binary/BCD_to_Binary_Design.v
@@ -1,1 +1,14 @@
+module BCD_to_Binary (BCD,binary);
+  input [15:0] BCD;
+  output reg [15:0] binary;
+  
+  //wire [3:0] ones,tens,hunds,thous;
+  //try some other logics later
 
+ 
+  always@(*)begin
+    binary=(BCD[3:0]*1)+(BCD[7:4]*10)+(BCD[11:8]*100)+(BCD[15:12]*1000);
+   
+ end
+  
+endmodule
