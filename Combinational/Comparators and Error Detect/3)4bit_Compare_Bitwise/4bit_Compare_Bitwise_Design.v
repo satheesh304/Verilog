73eeed793b56c4fd276a66b7bb4f7f19e909e9bd
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 12:59:28 +0530

    Update 4bit_Compare_Bitwise_Design.v

--- Combinational/Comparators and Error Detect/3)4bit_Compare_Bitwise/4bit_Compare_Bitwise_Design.v
+++ Combinational/Comparators and Error Detect/3)4bit_Compare_Bitwise/4bit_Compare_Bitwise_Design.v
@@ -1,1 +1,16 @@
-
+module Compare_4bit_bitwise(a,b,o);
+  input [3:0] a,b;
+  output reg [1:0] o;
+  
+  always @(*)
+    o = (a[3]>b[3])?2'b01 :
+        (a[3]<b[3])?2'b10 :
+        (a[2]>b[2])?2'b01 :
+        (a[2]<b[2])?2'b10 :
+        (a[1]>b[1])?2'b01 :
+        (a[1]<b[1])?2'b10 :
+        (a[0]>b[0])?2'b01 :
+        (a[0]<b[0])?2'b10 :
+                     2'b11;     
+               
+endmodule
