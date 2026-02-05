4b59addd7c4d0166ffb409cb6ebd9e6a06d783be
Satheesh <satheeshnarayan05@gmail.com>
2026-02-05 06:06:16 +0530

    Update Gray_to_Binary_Tb.v

--- Combinational/CODE CONVERTERS/Gray_to_Binary/Gray_to_Binary_Tb.v
+++ Combinational/CODE CONVERTERS/Gray_to_Binary/Gray_to_Binary_Tb.v
@@ -1,1 +1,24 @@
-
+//Test Bench
+module Gray_to_Binary_Tb;
+  reg [3:0] a;
+  wire [3:0] o;
+  integer i;
+  
+ Gray_to_Binary b1(a,o);
+  
+  initial begin
+ 
+    $monitor($time,"\tA=%b\t||\tOUTPUT=%b",a,o);
+    for(i=0;i<16;i=i+1)
+      begin
+      a=i;
+      #10;
+      end
+  end
+   initial begin
+    $dumpfile("mux.vcd");
+    $dumpvars;
+    #1000
+    $finish;
+  end
+endmodule
