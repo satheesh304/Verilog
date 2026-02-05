e80fcd3797828958605b4a1e165451af81e7337d
Satheesh <satheeshnarayan05@gmail.com>
2026-02-06 02:08:19 +0530

    Update 4:2_Priority_Encoder_Design.v

--- Combinational/Encoder and Decoder/4:2_Priority Encoder/4:2_Priority_Encoder_Design.v
+++ Combinational/Encoder and Decoder/4:2_Priority Encoder/4:2_Priority_Encoder_Design.v
@@ -1,6 +1,12 @@
 module Priority_Encoder(a,o);
   input [3:0] a;
   output [1:0] o;
+
   
+  //assign o = (a[3]) ? 2'b11 :         
+             //(a[2]) ? 2'b10 :           
+             //(a[1]) ? 2'b01 :
+             //(a[0]) ? 2'b00 :
+             //         2'bxx;
 assign o = (a[3])?2'b11:((a[2])?2'b10:((a[1])?2'b01:2'b00));
 endmodule
