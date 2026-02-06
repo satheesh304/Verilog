4e21e3584da1663bbd384bb0d3beb9806ba6a237
Satheesh <satheeshnarayan05@gmail.com>
2026-02-06 17:30:09 +0530

    Update 8x1_Mux(8 signals)_Design.v

--- Combinational/MUX and DeMUX/8x1_Mux(8 signals)/8x1_Mux(8 signals)_Design.v
+++ Combinational/MUX and DeMUX/8x1_Mux(8 signals)/8x1_Mux(8 signals)_Design.v
@@ -4,15 +4,16 @@ module mux(a,b,c,d,e,f,g,h,Sel,out);
   output [2:0] out;
   wire [7:0] A [2:0];
 
-   assign A[0]=a;
-   assign A[1]=b;
-   assign A[2]=c;
-   assign A[3]=d;
-   assign A[4]=e;
-   assign A[5]=f;
-   assign A[6]=g;
-   assign A[7]=h;
-    
+  always @(*) begin
+    A[0]=a;
+    A[1]=b;
+    A[2]=c;
+    A[3]=d;
+    A[4]=e;
+    A[5]=f;
+    A[6]=g;
+    A[7]=h; 
+  end
   
     assign out=A[Sel];//part select
 
