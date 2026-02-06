d49301a9e2a3653e64bebb0d1229f1f65a9cd272
Satheesh <satheeshnarayan05@gmail.com>
2026-02-06 16:23:39 +0530

    Update D_FF_Async_Reset.v

--- Sequential/2)FlipFlops/1)D_Flipflop/D_FF_Async_Reset.v
+++ Sequential/2)FlipFlops/1)D_Flipflop/D_FF_Async_Reset.v
@@ -1,1 +1,12 @@
-
+module d_ff(d,clk,rst,q);
+  input d,clk,rst;
+  output reg q;
+  //always @(posedge clk)
+  
+  always @(posedge clk or posedge rst)begin 
+    if (rst)
+      q<=1'b0;
+    else
+      q<=d;
+  end
+endmodule
