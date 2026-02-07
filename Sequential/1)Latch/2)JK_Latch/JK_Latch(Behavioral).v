4c40461770c37c39659657ac69ec11fe35cc4eca
Satheesh <satheeshnarayan05@gmail.com>
2026-02-07 23:27:09 +0530

    Update JK_Latch(Behavioral).v

--- Sequential/1)Latch/2)JK_Latch/JK_Latch(Behavioral).v
+++ Sequential/1)Latch/2)JK_Latch/JK_Latch(Behavioral).v
@@ -1,1 +1,17 @@
+module JK_latch_rst(input J,K,reset, output reg Q,output Qb);
+  
+  always @(*) begin
+    if (reset)
+      Q = 0;
+    else begin
+      case ({J,K})
+        2'b00: Q=Q;     // hold
+        2'b01: Q=0;     // reset
+        2'b10: Q=1;     // set
+        2'b11: Q=~Q;    // toggle
+      endcase
+      end
+  end
+  assign Qb = ~Q;
+endmodule
 
