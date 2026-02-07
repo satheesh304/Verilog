cbd4f6d2bc5e39d4c7b74c75f2b6fbdb76110995
Satheesh <satheeshnarayan05@gmail.com>
2026-02-07 22:58:35 +0530

    Update SR_FF_Async_Reset(active_high).v

--- Sequential/2)FlipFlops/4)SR_Flipflop/SR_FF_Async_Reset(active_high).v
+++ Sequential/2)FlipFlops/4)SR_Flipflop/SR_FF_Async_Reset(active_high).v
@@ -1,1 +1,18 @@
 
+module SR_ff(S, R, clk, rst, q);
+  input S, R, clk, rst;
+  output reg q;
+
+  always @(posedge clk or posedge rst) begin
+    if (rst)
+      q <= 1'b0;
+    else
+      case ({S,R})
+        2'b00: q <= q;      
+        2'b01: q <= 1'b0; 
+        2'b10: q <= 1'b1;  
+        2'b11: q <= 1'bx;  
+        default: q <= 1'bx;
+      endcase
+  end
+endmodule
