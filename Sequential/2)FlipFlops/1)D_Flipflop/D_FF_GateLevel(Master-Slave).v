911c3f69fc303de64bd2f863e5266fae30efa690
Satheesh <satheeshnarayan05@gmail.com>
2026-02-07 23:16:19 +0530

    Update D_FF_GateLevel(Master-Slave).v

--- Sequential/2)FlipFlops/1)D_Flipflop/D_FF_GateLevel(Master-Slave).v
+++ Sequential/2)FlipFlops/1)D_Flipflop/D_FF_GateLevel(Master-Slave).v
@@ -1,1 +1,11 @@
+module D_ff_rst(input D, clk, reset, output Q, Qb);
+  wire Dn, S, R;
+//Asynchronous rest
+  not  (Dn, D);
+  nand (S, D,  clk);
+  nand (R, Dn, clk);
+
+  nand (Q,  S, Qb, reset);
+  nand (Qb, R, Q,  reset);
+endmodule
 
