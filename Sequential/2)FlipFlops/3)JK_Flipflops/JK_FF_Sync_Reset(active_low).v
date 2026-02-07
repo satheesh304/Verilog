ab95e602e6e6d25c66a8ec51ca8d82e924302df1
Satheesh <satheeshnarayan05@gmail.com>
2026-02-07 22:30:38 +0530

    Update JK_FF_Sync_Reset(active_low).v

--- Sequential/2)FlipFlops/3)JK_Flipflops/JK_FF_Sync_Reset(active_low).v
+++ Sequential/2)FlipFlops/3)JK_Flipflops/JK_FF_Sync_Reset(active_low).v
@@ -1,1 +1,16 @@
-
+module JK_ff(J,K,clk,rst,q);
+  input J,K,clk,rst;
+  output reg q;
+  always @(posedge clk or negedge rst) begin
+    if (!rst)
+      q<=1'b0;
+    else
+      case({J,K})
+        2'b00:q<=q;
+        2'b01:q<=1'b0;
+        2'b10:q<=1'b1;
+        2'b11:q<=~q;
+        default:q<=1'bx;
+      endcase
+  end
+endmodule
