ee8d0e9c6272772baf3cbdfb1093bc88d128c02f
Satheesh <satheeshnarayan05@gmail.com>
2026-02-07 22:59:53 +0530

    Update SR_FF_Testbench.v

--- Sequential/2)FlipFlops/4)SR_Flipflop/SR_FF_Testbench.v
+++ Sequential/2)FlipFlops/4)SR_Flipflop/SR_FF_Testbench.v
@@ -1,1 +1,37 @@
+module SRff_TB;
+  reg S, R, clk, rst;
+  wire q;
+
+  SR_ff dut (S, R, clk, rst, q);
+  always 
+    #5 clk = ~clk;
+
+  initial begin
+    S = 0; 
+    R = 0; 
+    rst = 0; 
+    clk = 0;
+
+    #10 rst = 1;   
+    #10 S=0; R=0;   // HOLD
+    #10 S=0; R=1;   // RESET
+    #10 S=1; R=0;   // SET
+    #10 S=1; R=1;   // INVALID
+    #10 S=0; R=0;   // HOLD
+    #10 S=0; R=1;   // RESET
+    #10 S=1; R=0;   // SET
+    #10 S=1; R=1;   // INVALID
+
+    #20 $finish;
+  end
+  initial begin
+    $monitor($time,
+      "\tS=%b\tR=%b\tClock=%b\tReset=%b\tQ=%b",
+       S, R, clk, rst, q);
+  end
+  initial begin
+    $dumpfile("SR_ff.vcd");
+    $dumpvars;
+  end
+endmodule
 
