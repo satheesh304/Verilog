c501fe91f758f0a72e73f234c983806ab9d2095e
Satheesh <satheeshnarayan05@gmail.com>
2026-02-07 22:47:46 +0530

    Update JK_FF_Testbench.v

--- Sequential/2)FlipFlops/3)JK_Flipflops/JK_FF_Testbench.v
+++ Sequential/2)FlipFlops/3)JK_Flipflops/JK_FF_Testbench.v
@@ -1,1 +1,40 @@
+module JKff_TB();
+  reg J, K, clk, rst;
+  wire q;
+
+  JK_ff dut1(J, K, clk, rst, q);
+  
+  always #10 clk = ~clk;
+  initial begin
+    J = 0; 
+    K = 0; 
+    rst = 0; 
+    clk = 0;
+    
+    #10 rst=1;
+    #10 rst = 0;   
+    #10 J = 0; K = 0;   // HOLD
+    #10 J = 0; K = 1;   // RESET
+    #10 J = 1; K = 0;   // SET
+    #10 J = 1; K = 1;   // TOGGLE
+
+    #10 J = 0; K = 0;   // HOLD
+    #10 J = 0; K = 1;   // RESET
+    #10 J = 1; K = 0;   // SET
+    #10 J = 1; K = 1;   // TOGGLE
+
+    #20 $finish;
+  end
+
+  initial begin
+    $monitor($time,
+      "\tJ=%b\tK=%b\tClock=%b\tReset=%b\tQ=%b",J, K, clk, rst, q);
+  end
+
+  initial begin
+    $dumpfile("JK_ff.vcd");
+    $dumpvars;
+  end
+
+endmodule
 
