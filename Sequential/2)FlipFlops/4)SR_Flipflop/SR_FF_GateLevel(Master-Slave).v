54afabf0547db56fcda6be4dd770e64d41fe57fc
Satheesh <satheeshnarayan05@gmail.com>
2026-02-07 23:39:40 +0530

    Update SR_FF_GateLevel(Master-Slave).v

--- Sequential/2)FlipFlops/4)SR_Flipflop/SR_FF_GateLevel(Master-Slave).v
+++ Sequential/2)FlipFlops/4)SR_Flipflop/SR_FF_GateLevel(Master-Slave).v
@@ -1,9 +1,13 @@
-module SR_ff(input S, R, clk, reset, output Q, Qb);
-  wire Sg, Rg;
-//Asynchronous reset
-  nand (Sg, S, clk);
-  nand (Rg, R, clk);
-
-  nand (Q,  Sg, Qb, reset);
-  nand (Qb, Rg, Q,  reset);
+module SR_ff_rst(input S, R, clk, reset, output Q, Qb);
+  //Asynchonouos reset
+  wire Sm, Rm;
+  wire Qm, Qmb;
+//master
+  nand (Sm, S, clk);
+  nand (Rm, R, clk);
+  nand (Qm,  Sm, Qmb);
+  nand (Qmb, Rm, Qm);
+//Slave
+  nand (Q,  Qm,  ~clk, Qb, reset);
+  nand (Qb, Qmb, ~clk, Q,  reset);
 endmodule
