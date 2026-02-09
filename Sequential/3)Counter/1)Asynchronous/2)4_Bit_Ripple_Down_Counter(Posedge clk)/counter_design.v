ceb8eb3e6301fb3ef83bdc68fee4c0417f90e6ea
Satheesh <satheeshnarayan05@gmail.com>
2026-02-09 15:40:42 +0530

    Update counter_design.v

--- Sequential/3)Counter/1)Asynchronous/2)4_Bit_Ripple_Down_Counter(Posedge clk)/counter_design.v
+++ Sequential/3)Counter/1)Asynchronous/2)4_Bit_Ripple_Down_Counter(Posedge clk)/counter_design.v
@@ -3,8 +3,8 @@ module counter(T,clk,rst,Q);
   output reg [3:0] Q;
   
   t_ff T0(T,clk,rst,Q[0]);
-  t_ff T0(T,Q[0],rst,Q[1]);
-  t_ff T0(T,Q[1],rst,Q[2]);
-  t_ff T0(T,Q[2],rst,Q[3]);
+  t_ff T1(T,Q[0],rst,Q[1]);
+  t_ff T2(T,Q[1],rst,Q[2]);
+  t_ff T3(T,Q[2],rst,Q[3]);
   
 endmodule
