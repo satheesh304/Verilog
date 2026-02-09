fbd6583cc0eaa698206e3994da4a5146fc332759
Satheesh <satheeshnarayan05@gmail.com>
2026-02-09 14:43:18 +0530

    Update T_latch_Behavioral.v

--- Sequential/1)Latch/4)T_Latch/T_latch_Behavioral.v
+++ Sequential/1)Latch/4)T_Latch/T_latch_Behavioral.v
@@ -1,1 +1,10 @@
-
+module D_latch(input T,clk,reset,output reg q);
+  always @(clk or d or reset) begin
+    if (reset)
+      q<=0;
+    if(clk)
+      q<=T?~q:q;
+    else 
+      q<=q;
+  end
+endmodule
