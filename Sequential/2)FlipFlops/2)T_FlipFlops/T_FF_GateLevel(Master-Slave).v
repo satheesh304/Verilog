d1183bc51628c0db1b1006d815889b2319490831
Satheesh <satheeshnarayan05@gmail.com>
2026-02-09 11:37:38 +0530

    Update T_FF_GateLevel(Master-Slave).v

--- Sequential/2)FlipFlops/2)T_FlipFlops/T_FF_GateLevel(Master-Slave).v
+++ Sequential/2)FlipFlops/2)T_FlipFlops/T_FF_GateLevel(Master-Slave).v
@@ -1,13 +1,17 @@
 module T_ff(input T, clk, reset, output Q, Qb);
   //asynchronous reset
   wire Sm, Rm;
+  wire Ss, Rs;
   wire Qm, Qmb;
 //master
   nand (Sm, T, Qb, clk);
   nand (Rm, T, Q,  clk);
   nand (Qm,  Sm, Qmb);
   nand (Qmb, Rm, Qm);
-//slave
-  nand (Q,  Qm,  ~clk, Qb, reset);
-  nand (Qb, Qmb, ~clk, Q,  reset);
+  
+//slave latch
+  nand (Q,  Ss, Qm, ~clk);
+  nand (Qb, Rs, Qmb, ~clk);
+  nand (Q,  Qm, Qb, reset);
+  nand (Qb, Qmb, Q,  reset);
 endmodule
