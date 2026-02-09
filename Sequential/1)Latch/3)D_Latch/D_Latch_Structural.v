cdaabbd4e3767bb716c9be74034028625803501e
Satheesh <satheeshnarayan05@gmail.com>
2026-02-09 13:10:20 +0530

    Update D_Latch_Structural.v

--- Sequential/1)Latch/3)D_Latch/D_Latch_Structural.v
+++ Sequential/1)Latch/3)D_Latch/D_Latch_Structural.v
@@ -1,8 +1,8 @@
-module D_latch(input D,clk, reset, output Q, Qb);
-  wire S, R,Dn;
+module D_latch(input D,clk, reset, output Q);
+  wire S, R,Dn,Qb;
   not(Dn,D);
   nand (S, D, Qb,clk);
   nand (R, Dn, Q,clk);
-  nand (Q,  S, Qb, reset);
+  nand (Q,  S, Qb);
   nand (Qb, R, Q,  reset);
 endmodule
