a8bec04b56e7c212e5a68792823a719fb45f1be4
Satheesh <satheeshnarayan05@gmail.com>
2026-02-09 14:38:44 +0530

    Create T_Latch_Testbench.v

---
+++ Sequential/1)Latch/4)T_Latch/T_Latch_Testbench.v
@@ -0,0 +1,37 @@
+module T_latch_tb;
+  wire q, qb;
+  reg  T, reset, clk;
+
+  T_latch d1(T, clk, reset, q);
+  
+  always #5 clk = ~clk;
+
+  initial begin
+    clk = 0;
+    $monitor($time,"\tT=%b\treset=%b\tclk=%b\tQ=%b", T, reset, clk, q);
+
+    
+    reset = 1; T = 0;
+    #10 reset = 0;
+
+    
+    #10 T = 1;  
+    #10 T = 0; 
+    #10 T = 1;
+    #10 T = 1;   
+    #10 T = 0;  
+    #10 T = 0;   
+    #10 T = 1;  
+    #10 T = 0;   
+    #10 T = 1;   
+    #10 T = 0;   
+    #10 T = 1;  
+    #10 T = 0;   
+  end
+
+  initial begin
+    $dumpfile("D_latch.vcd");
+    $dumpvars;
+    #200 $finish;
+  end
+endmodule
