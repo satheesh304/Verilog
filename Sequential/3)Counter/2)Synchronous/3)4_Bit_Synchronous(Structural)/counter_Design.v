4144d2d77baa392ca2a9e2a6525ef8b43707c458
Satheesh <satheeshnarayan05@gmail.com>
2026-02-11 11:40:03 +0530

    Update counter_Design.v

--- Sequential/3)Counter/2)Synchronous/3)4_Bit_Synchronous(Structural)/counter_Design.v
+++ Sequential/3)Counter/2)Synchronous/3)4_Bit_Synchronous(Structural)/counter_Design.v
@@ -1,1 +1,11 @@
+//synchronous counter structural
 
+module Counter_gate(input t,clk,rst ,output [3:0] Q);
+ 
+  t_ff t0(t,clk,rst,Q[0]);
+  t_ff t1(Q[0],clk,rst,Q[1]);
+  t_ff t2(Q[0]&Q[1],clk,rst,Q[2]);
+  t_ff t3(Q[2]&Q[0]&Q[1],clk,rst,Q[3]);
+endmodule
+  
+  
