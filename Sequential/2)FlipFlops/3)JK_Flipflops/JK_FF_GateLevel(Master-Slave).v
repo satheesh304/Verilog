217bc3503f45d38d2719ad347ab6b2ae187a9621
Satheesh <satheeshnarayan05@gmail.com>
2026-02-11 09:55:09 +0530

    Update JK_FF_GateLevel(Master-Slave).v

--- Sequential/2)FlipFlops/3)JK_Flipflops/JK_FF_GateLevel(Master-Slave).v
+++ Sequential/2)FlipFlops/3)JK_Flipflops/JK_FF_GateLevel(Master-Slave).v
@@ -12,6 +12,6 @@ module JK_ff(input J, K, clk, reset, output Q, Qb);
   // Slave latch with reset
   nand (Ss, Qm, ~clk);
   nand (Rs, Qmb ,~clk);
-  nand (Q, Ss ,Qb, reset);
-  nand (Qb,Rs,  Q,  reset);
-endmodule
+  nand (Q, Ss ,Qb);
+  nand (Qb,Rs,  Q,~reset);//active high reset reset=0 -normal operation
+endmodule                // reset=1 resets Qb=1 and Q=0
