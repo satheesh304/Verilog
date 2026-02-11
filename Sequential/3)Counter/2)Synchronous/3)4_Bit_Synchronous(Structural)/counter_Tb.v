eeb61c482e496c2f13b27edca8066b7b3d572372
Satheesh <satheeshnarayan05@gmail.com>
2026-02-11 11:41:00 +0530

    Create counter_Tb.v

---
+++ Sequential/3)Counter/2)Synchronous/3)4_Bit_Synchronous(Structural)/counter_Tb.v
@@ -0,0 +1,26 @@
+module Counter_Tb();
+  reg clk,rst,t;
+  wire [3:0] Q;
+  
+  Counter_gate r1(t,clk,rst,Q);
+   
+ initial clk=1'b1;
+ always 
+   #10 clk=~clk;
+ initial begin
+   rst=1'b1;
+   t=1'b1;
+   #10 rst=1'b0;
+  
+    #10 rst=1'b0;
+   #2500 $finish;
+  end
+  initial begin
+    $monitor($time,"\t Q=%b or  %d",Q,Q);
+    $dumpfile("count.vcd");
+  $dumpvars;
+  end
+endmodule
+  
+ 
+  
