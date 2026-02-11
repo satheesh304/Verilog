8d730ab05c5119d4e54e2dade2072874b0e3f1f1
Satheesh <satheeshnarayan05@gmail.com>
2026-02-11 10:06:12 +0530

    Update counter.v

--- Sequential/3)Counter/1)Asynchronous/5)4-Bit_ UP&DOWN_Counter/counter.v
+++ Sequential/3)Counter/1)Asynchronous/5)4-Bit_ UP&DOWN_Counter/counter.v
@@ -17,6 +17,29 @@ module Ripple_UPDOWN_4bit #(parameter UD=1)(clk,rst,Q);
   end
   
 endmodule  
+
+/*design style using kind of MUX behavior
+module Ripple_UPDOWN_4bit(
+  input clk,
+  input rst,
+  input UD,          // 1 = UP, 0 = DOWN
+  output [3:0] Q
+);
+
+  wire c1, c2, c3;
+
+  // Clock selection based on UP/DOWN
+  assign c1 =(UD)?~Q[0]:Q[0];
+  assign c2 =(UD)?~Q[1]:Q[1];
+  assign c3 =(UD)?~Q[2]:Q[2];
+
+  TFF t1(Q[0],1'b1,clk,rst);
+  TFF t2(Q[1],1'b1,c1,rst);
+  TFF t3(Q[2],1'b1,c2,rst);
+  TFF t4(Q[3],1'b1,c3,rst);
+
+endmodule
+*/
   
 module TFF(q,t,clk,rst);
 output reg q;
