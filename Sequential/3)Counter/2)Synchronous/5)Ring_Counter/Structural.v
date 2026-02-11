313f90dc9aba901ed9cf4e600662b2a83dbed08a
Satheesh <satheeshnarayan05@gmail.com>
2026-02-11 16:43:26 +0530

    Update Structural.v

--- Sequential/3)Counter/2)Synchronous/5)Ring_Counter/Structural.v
+++ Sequential/3)Counter/2)Synchronous/5)Ring_Counter/Structural.v
@@ -1,1 +1,28 @@
+module RING_gate(input clk,rst,output reg [3:0] Q);
 
+ 
+  d_ff1 d0(Q[3],clk,rst,Q[0]);
+  d_ff d1(Q[0],clk,rst,Q[1]);
+  d_ff d2(Q[1],clk,rst,Q[2]);
+  d_ff d3(Q[2],clk,rst,Q[3]);
+ 
+endmodule
+
+
+module d_ff1(input d,clk,rst, output reg q );
+  always @(posedge clk) begin
+    if (rst)
+      q<=1;
+  else
+    q<=d;
+  end
+endmodule
+
+module d_ff(input d,clk,rst, output reg q );
+  always @(posedge clk) begin
+    if (rst)
+      q<=0;
+  else
+    q<=d;
+  end
+endmodule
