661e0126c4a37f076d724ada4c89b5a428854f56
Satheesh <satheeshnarayan05@gmail.com>
2026-02-11 16:29:51 +0530

    Update counter_Tb.v

--- Sequential/3)Counter/2)Synchronous/5)Ring_Counter/counter_Tb.v
+++ Sequential/3)Counter/2)Synchronous/5)Ring_Counter/counter_Tb.v
@@ -1,1 +1,25 @@
+module Counter_Tb();
+  reg clk,rst;
+  wire [3:0] Q;
+  
+  RING_gate r1(clk,rst,Q);
+   
+ initial clk=1'b1;
+ always 
+   #10 clk=~clk;
+ initial begin
+   rst=1'b1;
+  
+   #10 rst=1'b0;
 
+   #2500 $finish;
+  end
+  initial begin
+    $monitor($time,"\t Q=%b or  %d RST=%B",Q,Q,rst);
+    $dumpfile("count.vcd");
+  $dumpvars;
+  end
+endmodule
+  
+ 
+  
