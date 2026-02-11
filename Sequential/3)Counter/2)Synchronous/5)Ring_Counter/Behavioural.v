dfee7746210d8dbb53e9ef069cc60d2e20b31156
Satheesh <satheeshnarayan05@gmail.com>
2026-02-11 16:57:23 +0530

    Update Behavioural.v

--- Sequential/3)Counter/2)Synchronous/5)Ring_Counter/Behavioural.v
+++ Sequential/3)Counter/2)Synchronous/5)Ring_Counter/Behavioural.v
@@ -1,6 +1,4 @@
 module RING_gate(input clk,rst,output reg [3:0] Q);
-
- 
   always @(posedge clk) begin
     if (rst) 
        Q<=4'b0001;
@@ -11,7 +9,20 @@ module RING_gate(input clk,rst,output reg [3:0] Q);
     Q[2]<=Q[1];
     Q[3]<=Q[2];
     end
-  
   end
   
 endmodule
+
+/*
+N BIT RING COUNTER USING CONCANTENATION AND PARAMETER
+
+
+module RING_gate #(parameter N=4) (input clk,rst, output reg [N-1:0] Q);
+
+  always @(posedge clk) begin
+    if (rst)
+      Q<={{(N-1){1'b0}},1'b1};
+    else
+      Q<={Q[N-2:0],Q[N-1]};
+  end
+endmodule*/
