064f83b24f0421a42946c631b045c58bbd7e90f1
Satheesh <satheeshnarayan05@gmail.com>
2026-02-11 15:47:21 +0530

    Update Behavioural.v

--- Sequential/3)Counter/2)Synchronous/5)Ring_Counter/Behavioural.v
+++ Sequential/3)Counter/2)Synchronous/5)Ring_Counter/Behavioural.v
@@ -1,1 +1,16 @@
-
+module JOHNSON_gate(input t,clk,rst ,output reg [3:0] Q);
+ 
+  always @(posedge clk) begin
+    if (rst) 
+      Q<=0;
+    else begin
+   
+    Q[0]<=Q[3];
+    Q[1]<=Q[0];
+    Q[2]<=Q[1];
+    Q[3]<=Q[2];
+    end
+  
+  end
+  
+endmodule
