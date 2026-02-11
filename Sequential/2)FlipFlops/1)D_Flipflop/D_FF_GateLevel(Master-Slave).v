a29a27021fb3d56acc20f87052b4f4510a41e306
Satheesh <satheeshnarayan05@gmail.com>
2026-02-11 09:59:59 +0530

    Update D_FF_GateLevel(Master-Slave).v

--- Sequential/2)FlipFlops/1)D_Flipflop/D_FF_GateLevel(Master-Slave).v
+++ Sequential/2)FlipFlops/1)D_Flipflop/D_FF_GateLevel(Master-Slave).v
@@ -1,22 +1,22 @@
 module d_ff(input D, clk, reset, output Q, Qb);
   wire Dn;
-  wire Sm, Rm;
-  wire Qm, Qmb;
-  wire Ss, Rs;
+  wire Sm,Rm;
+  wire Qm,Qmb;
+  wire Ss,Rs;
 //Asynchronous rest
-  not  (Dn, D);
+  not  (Dn,D);
 
   // Master
-  nand (Sm, D,  clk);
-  nand (Rm, Dn, clk);
-  nand (Qm,  Sm, Qmb);
-  nand (Qmb, Rm, Qm);
+  nand (Sm,D,clk);
+  nand (Rm,Dn,clk);
+  nand (Qm,Sm,Qmb);
+  nand (Qmb,Rm,Qm);
 
   // Slave
-  nand (Ss, Qm,  ~clk);
-  nand (Rs, Qmb, ~clk);
+  nand (Ss,Qm,~clk);
+  nand (Rs,Qmb,~clk);
 
   //Output
-  nand (Q,  Ss, Qb);
-  nand (Qb, Rs, Q,~reset);//active high reset reset=0 -normal operation
+  nand (Q,Ss,Qb);
+  nand (Qb,Rs,Q,~reset);//active high reset reset=0 -normal operation
 endmodule                // reset=1 resets Qb=1 and Q=0
