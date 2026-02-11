98347932b659cc161aca39c80e9fb54100509a25
Satheesh <satheeshnarayan05@gmail.com>
2026-02-11 11:07:44 +0530

    Update counter_Tb.v

--- Sequential/3)Counter/1)Asynchronous/6)Decade(BCD)_Counter/counter_Tb.v
+++ Sequential/3)Counter/1)Asynchronous/6)Decade(BCD)_Counter/counter_Tb.v
@@ -1,1 +1,25 @@
-
+module Counter_Tb();
+  reg clk,rst;
+  wire [3:0] Q;
+  
+  Decade r1(clk,rst,Q);
+   
+ initial clk=1'b1;
+ always 
+   #10 clk=~clk;
+ initial begin
+    rst=1'b1;
+   #1 rst=1'b0;
+  
+    #1 rst=1'b0;
+   #2500 $finish;
+  end
+  initial begin
+    $monitor($time,"\t Q=%b or  %d",Q,Q);
+    $dumpfile("count.vcd");
+  $dumpvars;
+  end
+endmodule
+  
+ 
+  
