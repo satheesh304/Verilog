905cea54f7e044f32847992f0d439bbc745c2c8f
Satheesh <satheeshnarayan05@gmail.com>
2026-02-11 11:08:14 +0530

    Update counter_design.v

--- Sequential/3)Counter/1)Asynchronous/6)Decade(BCD)_Counter/counter_design.v
+++ Sequential/3)Counter/1)Asynchronous/6)Decade(BCD)_Counter/counter_design.v
@@ -1,1 +1,38 @@
 
+//Asynchronous BCD counter
+module Decade(input clk,rst,output reg [3:0] Q);
+  wire decade_rst;
+  assign  decade_rst=rst |(Q[3]&~Q[2]&Q[1]&~Q[0]);
+  
+  always @(posedge clk or posedge decade_rst) begin
+    if(decade_rst)
+      Q[0]<=1'b0;
+    else
+      Q[0]<=~Q[0];
+  end
+  
+  always @(posedge ~Q[0] or posedge decade_rst) begin
+    if(decade_rst)
+      Q[1]<=1'b0;
+    else
+      Q[1]<=~Q[1];
+  end
+  
+  always @(posedge ~Q[1] or posedge decade_rst) begin
+    if(decade_rst)
+      Q[2]<=1'b0;
+    else
+      Q[2]<=~Q[2];
+  end
+  
+  always @(posedge ~Q[2] or posedge decade_rst) begin
+    if(decade_rst)
+      Q[3]<=1'b0;
+    else
+      Q[3]<=~Q[3];
+  end
+
+endmodule
+
+
+
