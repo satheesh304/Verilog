81ea659e33be6106cf5ef88594176bef75f93bc9
Satheesh <satheeshnarayan05@gmail.com>
2026-02-12 19:19:56 +0530

    Update shift_register_Tb.v

--- Sequential/4)Registers/2)Multi-Mode_Shift _Registers/2)Universal_Shift_Register/shift_register_Tb.v
+++ Sequential/4)Registers/2)Multi-Mode_Shift _Registers/2)Universal_Shift_Register/shift_register_Tb.v
@@ -1,8 +1,8 @@
 //Testbench
 module SHIFTREGISTER_Tb ;
-  reg [3:0]Data;
+  reg [3:0]P_in;
   reg [2:0] MODE;
-  reg clk,rst;
+  reg S_in,clk,rst;
   wire  serial_Q;
   wire [3:0] parallel_Q;
   
@@ -12,12 +12,12 @@ module SHIFTREGISTER_Tb ;
  always 
    #5 clk=~clk;
 initial begin
-  rst=1;#15 rst=0; Data=4'b0000;MODE=3'd0;
-  #30 MODE=3'd3;Data=4'b0111;
-  #30 MODE=3'd1;
-  #30 MODE=3'd3;Data=4'b0111; 
+  rst=1;#15 rst=0; P_in=4'b0000;MODE=3'd0;
+  #30 MODE=3'd3;P_in=4'b0111;
+  #30 MODE=3'd1;S_in=1'b1;
+  #30 MODE=3'd3;P_in=4'b0111; 
   #10 MODE=3'D4;
-  #30 MODE=3'd2;
+  #30 MODE=3'd2;S_in=1'b0;
 
   #100 $finish;
 end
