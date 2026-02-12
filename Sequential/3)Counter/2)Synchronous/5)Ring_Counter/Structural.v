57cf0c5c1843e0e5e3c23e43e5ba2bb951d26643
Satheesh <satheeshnarayan05@gmail.com>
2026-02-12 10:05:10 +0530

    Update Structural.v

--- Sequential/3)Counter/2)Synchronous/5)Ring_Counter/Structural.v
+++ Sequential/3)Counter/2)Synchronous/5)Ring_Counter/Structural.v
@@ -1,4 +1,15 @@
-module RING_gate(input clk,rst,output reg [3:0] Q);
+module RING_gate(input clk,rst,preset,output reg [3:0] Q);
+
+ 
+  d_ff d0(Q[3],clk,preset,Q[0]);
+  d_ff d1(Q[0],clk,rst,Q[1]);
+  d_ff d2(Q[1],clk,rst,Q[2]);
+  d_ff d3(Q[2],clk,rst,Q[3]);
+ 
+endmodule
+
+
+/*module RING_gate(input clk,rst,preset,output reg [3:0] Q);
 
  
   d_ff1 d0(Q[3],clk,rst,Q[0]);
@@ -8,7 +19,6 @@ module RING_gate(input clk,rst,output reg [3:0] Q);
  
 endmodule
 
-
 module d_ff1(input d,clk,rst, output reg q );
   always @(posedge clk) begin
     if (rst)
@@ -16,13 +26,15 @@ module d_ff1(input d,clk,rst, output reg q );
   else
     q<=d;
   end
-endmodule
+endmodule*/
 
-module d_ff(input d,clk,rst, output reg q );
+module d_ff(input d,clk,preset,rst, output reg q );
   always @(posedge clk) begin
     if (rst)
       q<=0;
-  else
-    q<=d;
-  end
+   else if (preset)
+      q<=1;
+   else 
+      q<=d;
+   end
 endmodule
